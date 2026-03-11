9d570fd269a482efd2df82e4b3f15e4a71095dbb
lumigj <lumigj02@gmail.com>
2026-03-12 02:07:11 +0800

    还是分开才行不然on flag和实际状态不匹配

--- PE03/PE03-1-1.py
+++ PE03/PE03-1-1.py
@@ -39,104 +39,105 @@ t = time.monotonic()
 c = 0
 try:
     while True:
-        # 松开才改
-        if GPIO.input(butRedPin):
-            if r_state:
-                r_state = False
-                r_on = not r_on
-                GPIO.output(ledRedPin, r_on)
-        else:
-            if not r_state:
-                r_state = True
+        if f_step == 0:
+            # 松开才改
+            if GPIO.input(butRedPin):
+                if r_state:
+                    r_state = False
+                    r_on = not r_on
+                    GPIO.output(ledRedPin, r_on)
+            else:
+                if not r_state:
+                    r_state = True
 
-        # 按下就改
-        if GPIO.input(butGreenPin):
-            if g_state:
-                g_state = False
-        else:
-            if not g_state:
-                g_state = True
-                g_on = not g_on
-                GPIO.output(ledGreenPin, g_on)
+            # 按下就改
+            if GPIO.input(butGreenPin):
+                if g_state:
+                    g_state = False
+            else:
+                if not g_state:
+                    g_state = True
+                    g_on = not g_on
+                    GPIO.output(ledGreenPin, g_on)
 
-        # 亮只需按下，灭却等松开
-        if GPIO.input(butBluePin):
-            if b_state:
-                b_state = False
-                if b_wait_off:
-                    b_wait_off = False
-                    b_on = False
-                    GPIO.output(ledBluePin, b_on)
-        else:
-            if not b_state:
-                b_state = True
-                if b_on:
-                    b_wait_off = True
-                else:
-                    b_on = True
-                    GPIO.output(ledBluePin, b_on)
+            # 亮只需按下，灭却等松开
+            if GPIO.input(butBluePin):
+                if b_state:
+                    b_state = False
+                    if b_wait_off:
+                        b_wait_off = False
+                        b_on = False
+                        GPIO.output(ledBluePin, b_on)
+            else:
+                if not b_state:
+                    b_state = True
+                    if b_on:
+                        b_wait_off = True
+                    else:
+                        b_on = True
+                        GPIO.output(ledBluePin, b_on)
 
-
-        if GPIO.input(butFourthPin):
-            if f_state:
-                f_state = False
-                #进入那个pattern模式 用读秒+状态机的方式
-                if f_step == 0:
-                    f_step = 1
-                    t = time.monotonic()
-                    GPIO.output(ledRedPin, False)
-                    GPIO.output(ledGreenPin, False)
-                    GPIO.output(ledBluePin, False)
-                else:
-                    f_step = 0
         else:
-            if not f_state:
-                f_state = True
-        if f_step == 1 and time.monotonic() > t + 1:
-            f_step = 2
-            t = time.monotonic()
-            GPIO.output(ledRedPin, True)
-        elif f_step == 2 and time.monotonic() > t + 1:
-            f_step = 3
-            t = time.monotonic()
-            GPIO.output(ledRedPin, False)
-            GPIO.output(ledGreenPin, True)
-        elif f_step == 3 and time.monotonic() > t + 1:
-            f_step = 4
-            t = time.monotonic()
-            GPIO.output(ledGreenPin, False)
-            GPIO.output(ledBluePin, True)
-        elif f_step == 4 and time.monotonic() > t + 1:
-            f_step = 5
-            t = time.monotonic()
-            GPIO.output(ledBluePin, False)
-        elif f_step == 5 and time.monotonic() > t + 1:
-            f_step = 6
-            t = time.monotonic()
-            GPIO.output(ledRedPin, True)
-            GPIO.output(ledGreenPin, True)
-            GPIO.output(ledBluePin, True)
-        elif f_step == 6 and time.monotonic() > t + 0.3:
-            f_step = 7
-            t = time.monotonic()
-            c += 1
-            GPIO.output(ledRedPin, False)
-            GPIO.output(ledGreenPin, False)
-            GPIO.output(ledBluePin, False)
-        elif f_step == 7 and time.monotonic() > t + 0.3:
-            if c < 3:
+            if GPIO.input(butFourthPin):
+                if f_state:
+                    f_state = False
+                    #进入那个pattern模式 用读秒+状态机的方式
+                    if f_step == 0:
+                        f_step = 1
+                        t = time.monotonic()
+                        GPIO.output(ledRedPin, False)
+                        GPIO.output(ledGreenPin, False)
+                        GPIO.output(ledBluePin, False)
+                    else:
+                        f_step = 0
+            else:
+                if not f_state:
+                    f_state = True
+            if f_step == 1 and time.monotonic() > t + 1:
+                f_step = 2
+                t = time.monotonic()
+                GPIO.output(ledRedPin, True)
+            elif f_step == 2 and time.monotonic() > t + 1:
+                f_step = 3
+                t = time.monotonic()
+                GPIO.output(ledRedPin, False)
+                GPIO.output(ledGreenPin, True)
+            elif f_step == 3 and time.monotonic() > t + 1:
+                f_step = 4
+                t = time.monotonic()
+                GPIO.output(ledGreenPin, False)
+                GPIO.output(ledBluePin, True)
+            elif f_step == 4 and time.monotonic() > t + 1:
+                f_step = 5
+                t = time.monotonic()
+                GPIO.output(ledBluePin, False)
+            elif f_step == 5 and time.monotonic() > t + 1:
                 f_step = 6
                 t = time.monotonic()
                 GPIO.output(ledRedPin, True)
                 GPIO.output(ledGreenPin, True)
                 GPIO.output(ledBluePin, True)
-            else :
-                f_step = 1
+            elif f_step == 6 and time.monotonic() > t + 0.3:
+                f_step = 7
                 t = time.monotonic()
-                c = 0
+                c += 1
                 GPIO.output(ledRedPin, False)
                 GPIO.output(ledGreenPin, False)
                 GPIO.output(ledBluePin, False)
+            elif f_step == 7 and time.monotonic() > t + 0.3:
+                if c < 3:
+                    f_step = 6
+                    t = time.monotonic()
+                    GPIO.output(ledRedPin, True)
+                    GPIO.output(ledGreenPin, True)
+                    GPIO.output(ledBluePin, True)
+                else :
+                    f_step = 1
+                    t = time.monotonic()
+                    c = 0
+                    GPIO.output(ledRedPin, False)
+                    GPIO.output(ledGreenPin, False)
+                    GPIO.output(ledBluePin, False)
 
 
 
