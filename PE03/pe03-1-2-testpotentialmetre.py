12ae3b681602b1ed454a8c91617982afdadbdddd
lumigj <lumigj02@gmail.com>
2026-03-18 20:28:33 +0800

    让led.source被pot.values控制

--- PE03/pe03-1-2-testpotentialmetre.py
+++ PE03/pe03-1-2-testpotentialmetre.py
@@ -1,8 +1,20 @@
 import time
 
 from gpiozero import MCP3008
+from gpiozero import PWMLED
 
 pot = MCP3008(channel=0)
+
+led = PWMLED(21) # GPIO 21 or Pin 40
+led.on() # test - the led should be lit
+time.sleep(1)
+led.off() # test - the led should go off
+time.sleep(1)
+led.value = 0.5 # test - the led should be lit at half brightness
+time.sleep(1)
+led.off()
+time.sleep(1)
+led.source = pot.values # actual connecting of the LED to the potentiometer
 
 print('Program running... Press CTRL+C to exit')
 
@@ -10,10 +22,10 @@ print('Program running... Press CTRL+C to exit')
 try:
 
     while True:
-        print(pot.value)
-
-
         time.sleep(0.1)
 
 except KeyboardInterrupt:
     print('Program terminated!')
+
+except Exception as err:
+    print('An error has occurred: {}'.format(err))
